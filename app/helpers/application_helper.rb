c87ea13557ff699f82d44267e48e7d82db15f0e2
maangie <maangie906@gmail.com>
2015-05-08 17:41:15 +0900

    ブロック引数には「?」が使えない。らしい。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # 指定されたサーバの全ての通知先に、サーバの通知先を送信する
-  def send_server_status_to_advisories_helper(server, &send?)
+  def send_server_status_to_advisories_helper(server, &check_send)
     server.check_status
-    return unless send?(server.status.code)
+    return unless check_send(server.status.code)
     emails = server.advisories.map(&:email)
     NoticeMailer.sendmail_confirm(emails, server).deliver
   end
