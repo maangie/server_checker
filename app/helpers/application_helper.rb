21d8fb9af884adcf1bee040756f165c42d3ec31e
maangie <maangie906@gmail.com>
2015-11-20 15:46:32 +0900

    rescue check code for rake task.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   # 指定されたサーバの全ての通知先に、サーバの通知先を送信する
-  def send_server_status_to_advisories_helper(server)
+  def send_server_status_to_advisories_helper(server, &check_send)
     server.check_status
+    return if check_send && !check_send(server.status.code)
     emails = server.advisories.map(&:email)
     NoticeMailer.sendmail_confirm(emails, server).deliver
   end
